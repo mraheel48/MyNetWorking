4bbfff8d0b6ccbdecf8f1865503a599a9b783ed2
Muhammad Raheel <57173923+mraheel48@users.noreply.github.com>
2022-10-18 17:04:24 +0500

    under_30

--- app/src/main/java/com/example/mynetworking/MainActivity.kt
+++ app/src/main/java/com/example/mynetworking/MainActivity.kt
@@ -2,11 +2,22 @@ package com.example.mynetworking
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.example.networkinglibrary.NetWorking
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        NetWorking.getNetworkLiveData(applicationContext).observe(this) { isConnected ->
+            if (isConnected) {
+                Log.d("myNetWork", "Net Work is connect")
+            } else {
+                Log.d("myNetWork", "Net Work is not connect")
+            }
+        }
+
     }
 }
